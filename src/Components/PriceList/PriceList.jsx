3b19c79c8f7e07bc12b589e44acd427b38dc2439
mohammadibrahim20 <ibinfo2017bd@gmail.com>
2023-10-17 00:24:06 +0600

    2nd test

--- src/Components/PriceList/PriceList.jsx
+++ src/Components/PriceList/PriceList.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PriceList = () => {
     return (
         <div>
-            <h1 className='text text-6xl bg-purple-300 text-center'>Awesome Affordable</h1>
+            <h1 className='text text-6xl bg-purple-300 text-center'>Awesomefdfdf Affordable</h1>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In, suscipit aspernatur incidunt provident dolores est natus quidem tempora iste. Consectetur nostrum sed unde reiciendis earum recusandae. Ullam nisi modi perferendis dolorum ipsa libero debitis eius ipsam quod quidem vero rem deleniti impedit enim tenetur, doloribus laborum. Neque, tempora id molestiae similique nam quas, soluta placeat odit corrupti, nesciunt vitae amet ipsa! Fuga neque, illum tempora voluptatibus, repellat perferendis, praesentium ducimus commodi eum minima dicta fugiat! Asperiores repellendus perspiciatis fugit amet saepe? Iure minus cupiditate dolores iusto eveniet aperiam debitis sed ipsam molestiae esse? Sapiente quaerat velit voluptatem rerum at minus.</p>
         </div>
     );
